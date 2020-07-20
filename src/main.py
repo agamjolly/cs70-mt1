28e522998eb2d814b7692ff652fb7f18f645289b
agamjolly <agamjolly@berkeley.edu>
2020-07-21 02:40:44 +0530

    fixed graph

--- src/main.py
+++ src/main.py
@@ -7,20 +7,15 @@ import xlrd
 from collections import Counter
 from flask import *
 
-"""
-cs70 midterm 1 stats<br>
-last modified: {{ last }}<br>
-<br>
-mean: {{ mean }}<br>
-median: {{ median }}<br>
-standard deviation: {{ std }}<br>
-"""
 
 df = pd.read_excel('Book1.xlsx')
 a = df['marks'].tolist()
+com = df['comments'].tolist()
 app = Flask(__name__)
 path = os.path.getmtime('Book1.xlsx')
 last_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(path))
+
+com = [i for i in com if len(str(i))>3]
 
 def mean(data=a):  
     return float(sum(data)/len(data))
@@ -30,11 +25,7 @@ def standard_deviation(data=a):
     
 def median(data=a): 
     new_data = sorted(data)
-    med1 = new_data[len(new_data)//2]
-    if not len(data)%2: 
-        med2 = new_data[(len(data)//2)-1]
-        return float((med1+med2))/2
-    return med1
+    return statistics.median(new_data)
 
 def std_minus(data=a): 
     return [(round((median()-i*standard_deviation()),2)) for i in np.arange(0.5, 2.0, 0.5)]
@@ -52,47 +43,31 @@ plus = ["+0.5 STD: ", "+1.0 STD: ", "+1.5 STD: "]
 minus = ["-0.5 STD: ", "-1.0 STD: ", "-1.5 STD: "]
 
 
-score_wise = [0, 0, 0, 0, 0, 0, 0]
+score_wise = [0,0,0,0,0,0,0,0]
 
-for i in a: 
-    if i in range(10,20): 
+for i in a:
+    if int(i) in range(0,10):
         score_wise[0]+=1
-    elif i in range(20,30): 
+    elif int(i) in range(10,20): 
         score_wise[1]+=1
-    elif i in range(30,40): 
+    elif int(i) in range(20,30): 
         score_wise[2]+=1
-    elif i in range(40,50): 
+    elif int(i) in range(30,40): 
         score_wise[3]+=1
-    elif i in range(50,60): 
+    elif int(i) in range(40,50): 
         score_wise[4]+=1
-    elif i in range(60,70): 
+    elif int(i) in range(50,60): 
         score_wise[5]+=1
-    else: 
+    elif int(i) in range(60,70):
         score_wise[6]+=1
+    elif int(i) in range(70,100): 
+        score_wise[7]+=1
 
 
 @app.route('/')
 def index(): 
-    return render_template('index.html', mean=(round(mean(),2)), median=(round(median(),2)), mode=mode(), std=(round(standard_deviation(),2)), a=sorted(a), last=last_time, total=len(a), hi=max(a), lo=min(a), std_plus=dict(zip(plus, std_plus())), std_minus=dict(zip(minus, std_minus())), thresholds=[i for i in score_wise], numbers=score_wise)
+    return render_template('index.html', comments=com, mean=(round(mean(),2)), median=(round(median(),2)), mode=mode(), std=(round(standard_deviation(),2)), a=sorted(a), last=last_time, total=len(a), hi=max(a), lo=min(a), std_plus=dict(zip(plus, std_plus())), std_minus=dict(zip(minus, std_minus())), thresholds=[i for i in score_wise], numbers=score_wise)
 
 # Driver code
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-    """
-    print("------------------------------"+ "\nCS70 Midterm 1 Stats")
-    print("Last modified: " + str(last_time))
-    print("------------------------------")
-    print("Total entries: " + str(len(a)))  
-    print("Median: "+ str(round(median(),2)))
-    print("Mean: "+ str(round(mean(),2)))
-    print("Standard Deviation: "+ str(round(standard_deviation(),2)))
-    print("Highest Score: " + str(max(a))) 
-    print("Lowest Score: " + str(min(a)))
-    print("------------------------------")
-    for i in np.arange(0.5, 2.0, 0.5):
-        print("-" + str(i) + " STD: " + str(round((median()-i*standard_deviation()),2)))
-    print("------------------------------")
-    for i in np.arange(0.5, 2.0, 0.5):
-        print("+" + str(i) + " STD: " + str(round((median()+i*standard_deviation()),2)))
-    print()
-    """
